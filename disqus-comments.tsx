532ba70faa501a1deba489e728ffd9b7a0ce7e49
Batchor <batchfy@gmail.com>
2025-01-12 02:13:01 -0800

    disqus

---
+++ disqus-comments.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { DiscussionEmbed, CommentCount, Recommendations } from "disqus-react";
+import { Divider } from "@nextui-org/divider";
+
+const DisqusComments = ({ domain="batchfy.com", id, slug }: { domain?: string, id: string; slug: string }) => {
+    const disqusShortname = "batchfy";
+    const disqusConfig = {
+        url: `https://${domain}/${slug}`,
+        identifier: id,
+    };
+
+    return (
+        <div>
+            <Divider className="my-4" orientation="horizontal" />
+            <DiscussionEmbed config={disqusConfig} shortname={disqusShortname} />
+            <CommentCount
+                config={{ url: `https://${domain}/${slug}`, identifier: id }}
+                shortname="example"
+            />
+            <Recommendations
+                config={{ url: `https://${domain}/${slug}`, identifier: id }}
+                shortname={disqusShortname}
+            />
+        </div>
+    );
+};
+
+export default DisqusComments;
