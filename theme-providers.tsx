39100083b3685f226c7400c80b4b500feffddcd5
Anonymous <anonymous@example.com>
2025-02-11 15:00:06 -0800

    theme switch

--- theme-providers.tsx
+++ theme-providers.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import * as React from "react";
-import { HeroUIProvider } from "@heroui/system";
-import { useRouter } from "next/navigation";
 import { ThemeProvider } from "next-themes";
 
 type ThemeProviderProps = React.ComponentProps<typeof ThemeProvider>;
@@ -13,11 +11,7 @@ export interface ProvidersProps {
 }
 
 export function Providers({ children, themeProps }: ProvidersProps) {
-    const router = useRouter();
-
     return (
-        <HeroUIProvider navigate={router.push}>
-            <ThemeProvider {...themeProps}>{children}</ThemeProvider>
-        </HeroUIProvider>
+        <ThemeProvider {...themeProps}>{children}</ThemeProvider>
     );
 }
