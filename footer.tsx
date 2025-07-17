643f9eb1df718a76cba5d21ccacf96319f5da384
Kai Zhao <694691@qq.com>
2025-07-16 19:44:37 -0700

    fix navbar

--- footer.tsx
+++ footer.tsx
@@ -5,15 +5,16 @@ interface FooterProps {
     socials: string[]
     name: string
     domain: string
+    yearFrom?: number
     iconSize?: number
 }
 
-export const Footer: React.FC<FooterProps> = ({ socials, name, domain, iconSize = 23 }) => {
+export const Footer: React.FC<FooterProps> = ({ socials, name, domain, yearFrom=1990, iconSize = 23 }) => {
     const year = new Date().getFullYear()
     return (
         <footer className="flex w-full flex-col items-center justify-center space-x-4 pb-4">
             <div className="text-sm">
-                &copy;<span className="text-default-600">Copyright</span>&nbsp;2010~{year}&nbsp;
+                &copy;<span className="text-default-600">Copyright</span>&nbsp;{yearFrom}~{year}&nbsp;
                 <Link isExternal className="text-current" href={`https://${domain}`}>
                     <span className="text-primary text-sm">{name}</span>
                 </Link>
